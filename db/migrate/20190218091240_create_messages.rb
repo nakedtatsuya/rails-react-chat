f20f861cb399315b5089bdd2c379fa6cba751b49
tatsuya fukui <nakedtatsuya6@gmail.com>
2019-02-18 21:52:21 +0900

    initial commit

---
+++ db/migrate/20190218091240_create_messages.rb
@@ -0,0 +1,10 @@
+class CreateMessages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :messages do |t|
+      t.text :message
+      t.integer :from_id
+      t.integer :to_id
+      t.timestamps
+    end
+  end
+end
